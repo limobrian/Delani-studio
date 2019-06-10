3052c6f4a7dc131f77c33295afbfdd5020aad285
briankiplimo <limobrian290@gmail.com>
2019-06-10 11:53:42 +0300

    hovering

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,34 @@
+$(document).ready(function () {
+  $("#userInputs").submit(function (event) {
+    var name = $("#fName").val();
+    if (name != "") {
+      alert(name + " thanks for sending a message.");
+    } else {
+      alert("Please fill in your details");
+    }
+    event.preventDefault();
+  });
 
-$("#submit").click(function(event){
-  var name = $("#fName").val();
-  if(name != ""){
-    alert( name + " thanks for sending a message.");
-  }else {
-    alert("Please fill in your details");
-  }
-event.preventDefault();
+  //TOGGLE
+  $('.cont1').hide()
+  $('#tog1').click(function () {
+    $('.cont1').toggle();
+    $('.cont1').click(function () {
+      $('#tog1').toggle();
+    });
+  });
+  $('.cont2').hide()
+  $('#tog2').click(function () {
+    $('.cont2').toggle();
+    $('.cont2').click(function () {
+      $('#tog2').toggle();
+    });
+  });
+  $('.cont3').hide()
+  $('#tog3').click(function () {
+    $('.cont3').toggle();
+    $('.cont3').click(function () {
+      $('#tog3').toggle();
+    });
+  });
 });
-
-//TOGGLE
-$('.cont1').hide()
-$('#tog1').click(function(){
-  $('.cont1').toggle();
-  $('.cont1').click(function(){
-    $('#tog1').toggle();
-  });
- });
- $('.cont2').hide()
-$('#tog2').click(function(){
-  $('.cont2').toggle();
-  $('.cont2').click(function(){
-    $('#tog2').toggle();
-  });
- });
- $('.cont3').hide()
-$('#tog3').click(function(){
-  $('.cont').toggle();
-  $('.cont3').click(function(){
-    $('#tog3').toggle();
-  });
- });
