cb6810f0144b34853f6edf67ecb766f46bcd8a38
briankiplimo <limobrian290@gmail.com>
2019-06-10 11:23:27 +0300

    socialo

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,33 @@
-/*$(document).ready(function () {
-  $(".tog1").click(function () {
-    $(".cont1").toggle();
-  });
-});
-$(document).ready(function () {
-  $(".clickondevelopment").click(function () {
-    $(".development-hidden").toggle();
-    $(".development-showing").toggle();
-  });
-});
-$(document).ready(function () {
-  $(".clickonproduct").click(function () {
-    $(".product-hidden").toggle();
-    $(".product-showing").toggle();
-  });
-});**/
 
-/** forms */
-$("#submission").submit(function (event) {
-var data_1 = $("input:first").val();
-var data_2 = $("input#mail").val();
-if (data_1 && data_2 != "") {
-  $(".message").text("Thank you " + data_1 + " Your message has been received");
-  $(".message").removeClass("error");
-  $(".message").addClass("success");
-} else {
-  $(".message").text("Error!!! Please fill in all the details correctly");
-  $(".message").addClass("error");
-}
+$("#submit").click(function(event){
+  var name = $("#fName").val();
+  if(name != ""){
+    alert( name + " thanks for sending a message.");
+  }else {
+    alert("Please fill in your details");
+  }
 event.preventDefault();
 });
 
-});
 //TOGGLE
 $('.cont1').hide()
 $('#tog1').click(function(){
- $('.cont1').toggle();
- $('.cont1').click(function(){
-   $('#tog1').toggle();
+  $('.cont1').toggle();
+  $('.cont1').click(function(){
+    $('#tog1').toggle();
+  });
  });
-});
+ $('.cont2').hide()
+$('#tog2').click(function(){
+  $('.cont2').toggle();
+  $('.cont2').click(function(){
+    $('#tog2').toggle();
+  });
+ });
+ $('.cont3').hide()
+$('#tog3').click(function(){
+  $('.cont').toggle();
+  $('.cont3').click(function(){
+    $('#tog3').toggle();
+  });
+ });
